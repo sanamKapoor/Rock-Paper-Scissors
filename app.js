303063fff069fa55e1c464dde4f9447fb2e2720c
Willson Purl <purlwillson786@gmail.com>
2019-06-29 11:08:18 +0530

    js file

---
+++ app.js
@@ -0,0 +1,125 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+  player: 0,
+  computer: 0
+}
+
+//      Play Game
+
+function play(e){
+  restart.style.display = 'inline-block';
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  showWinner(winner, computerChoice);
+
+  //console.log(playerChoice, computerChoice, winner);
+
+}
+
+//            Get Computer Choice
+
+function getComputerChoice(){
+  const rand = Math.random();
+  if(rand < 0.3){
+    return 'rock';
+  }
+  else if(rand < 0.5){
+    return 'paper';
+  }
+  else{
+    return 'scissors';
+  }
+}
+
+
+//            Get Winner
+function getWinner(p, c){
+  if(p === c){
+    return 'draw';
+  }
+  else if(p === 'rock'){
+        if(c === 'paper'){
+          return 'computer';
+        }
+        else{
+          return 'player';
+        }
+  }
+  else if(p === 'paper'){
+        if(c === 'scissors'){
+          return 'computer';
+        }
+        else{
+          return 'player';
+        }
+  }
+  else if(p === 'scissors'){
+        if(c === 'rock'){
+          return 'computer';
+        }
+        else{
+          return 'player';
+        }
+  }
+}
+
+
+//          Show Winner
+function showWinner(winner, computerChoice){
+  if(winner === 'player'){
+    scoreboard.player++;
+    //    Show modal result
+    result.innerHTML =
+              `<h1 class="text-win">You Win</h1>
+              <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+              <p>Computer chose <strong>${computerChoice}</strong></p>`;
+  }
+  else if(winner === 'computer'){
+    scoreboard.computer++;
+    //    Show modal result
+    result.innerHTML =
+              `<h1 class="text-win">You Lose</h1>
+              <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+              <p>Computer chose <strong>${computerChoice}</strong></p>`;
+  }
+  else{
+    result.innerHTML =
+              `<h1 class="text-win">Draw Game</h1>
+              <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+              <p>Computer chose <strong>${computerChoice}</strong></p>`;
+  }
+
+  score.innerHTML = 
+  `<p class="p-01">Player: ${scoreboard.player}</p>
+  <p class="p-02">Computer: ${scoreboard.computer}</p>`;
+
+  modal.style.display = 'block';
+}
+
+//          Clear modal
+function clearModal(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+
+//          Restart Game
+function restartGame(){
+  scoreboard.player = 0;
+  scoreboard.computer = 0;
+  score.innerHTML = 
+  `<p class="p-01">Player: ${scoreboard.player}</p>
+  <p class="p-02">Computer: ${scoreboard.computer}</p>`;
+
+}
+
+//        Event
+
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
